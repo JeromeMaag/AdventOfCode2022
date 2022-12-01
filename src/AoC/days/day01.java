f1055f2cbf5ed1f733251607d8f93fa72a0202f2
Jerome Maag <maagjer@students.zhaw.ch>
2022-12-01 06:21:50 +0100

    Test and setup day1

--- src/AoC/days/day01.java
+++ src/AoC/days/day01.java
@@ -3,16 +3,38 @@ package AoC.days;
 import AoC.day;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class day01 implements day {
     @Override
     public String part1(List<String> input, List<String> input2) {
 
+        List<Integer> i = new ArrayList<>();
+        i.add(0);
+
+        for (String s: input2){
+            if(s.equals("")){
+                i.add(0);
+            }
+            else {
+                int x = i.get(i.size()-1);
+                x = x + Integer.valueOf(s);
+                i.remove(i.size()-1);
+                i.add(x);
+
+            }
+        }
 
 
-        int res = 0;
-        return String.valueOf(res);
+
+        System.out.println(i);
+
+
+        Collections.sort(i);
+        System.out.println(i);
+
+        return String.valueOf("1");
 
     }
 
