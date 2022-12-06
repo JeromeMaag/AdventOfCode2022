b1323cef7b08b9cf8da5ecafb379ae54ca708c1f
Jerome <maag.jerome@gmail.com>
2022-12-06 08:16:45 +0100

    solve day6

--- src/AoC/days/day06.java
+++ src/AoC/days/day06.java
@@ -8,10 +8,28 @@ import java.util.List;
 public class day06 implements day {
     @Override
     public String part1(List<String> input) {
-        int res = 0;
+        int res = 3;
+        boolean i = true;
 
         for(String s: input){
-            String[] sPars = s.trim().split(" ");
+            String[] sPars = s.trim().split("");
+            while (i){
+                res ++;
+                String one = sPars[res - 3];
+                String two = sPars[res - 2];
+                String three = sPars[res - 1];
+                String four = sPars[res];
+                if(one.equals(two) || one.equals(three) || one.equals(four) || two.equals(three) || two.equals(four) || three.equals(four)){
+                    i = true;
+                }else {
+                    i = false;
+                    res++;
+                }
+
+            }
+            break;
+
+
         }
 
 
@@ -20,7 +38,16 @@ public class day06 implements day {
 
     @Override
     public String part2(List<String> input) {
-        int res = 0;
-        return String.valueOf(res);
+        int res = 14;
+        String s = input.stream().map(e -> e.toString()).reduce("", String::concat);
+        for (int i = 0; i < s.length() - 14; i++) {
+            String slice = s.substring(i, i + 14);
+            if (slice.chars().distinct().count() == 14) {
+                return String.valueOf(res);
+            } else {
+                res++;
+            }
+        }
+        return s;
     }
 }
