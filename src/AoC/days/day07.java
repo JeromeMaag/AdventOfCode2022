74fdc2b33dabe3ae7afdbf4609ff59e2dce370ee
Jerome Maag <maagjer@students.zhaw.ch>
2022-12-08 11:33:00 +0100

    solve day 7 & 8

--- src/AoC/days/day07.java
+++ src/AoC/days/day07.java
@@ -2,25 +2,131 @@ package AoC.days;
 
 import AoC.day;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class day07 implements day {
+
     @Override
     public String part1(List<String> input) {
-        int res = 0;
+        HashMap<String, Object> root = new HashMap<>();
+        HashMap<String, Object> dirs = root;
+        Stack<HashMap<String, Object>> stack = new Stack<>();
 
-        for(String s: input){
-            String[] sPars = s.trim().split(" ");
+        for (String s : input) {
+            if (s.charAt(0) == '$') {
+                if (s.charAt(2) == 'c') {
+                    String dir = s.substring(5);
+                    if (dir.equals("/")) {
+                        dirs = root;
+                        stack.clear();
+                    } else if (dir.equals("..")) {
+                        dirs = stack.pop();
+                    } else {
+                        if (!dirs.containsKey(dir)) {
+                            dirs.put(dir, new HashMap<String, Object>());
+                        }
+                        stack.push(dirs);
+                        dirs = (HashMap<String, Object>) dirs.get(dir);
+                    }
+                }
+            } else {
+                String[] parts = s.split(" ");
+                String dirOrSize = parts[0];
+                String name = parts[1];
+                if (dirOrSize.equals("dir")) {
+                    if (!dirs.containsKey(name)) {
+                        dirs.put(name, new HashMap<String, Object>());
+                    }
+                } else {
+                    dirs.put(name, Integer.parseInt(dirOrSize));
+                }
+            }
         }
+        return String.valueOf(sumDirs(root)[1]);
+    }
 
-
-        return String.valueOf(res);
+    int[] sumDirs(HashMap<String, Object> dir) {
+        int size = 0;
+        int res = 0;
+        for (Object child : dir.values()) {
+            if (child instanceof HashMap) {
+                int[] s = sumDirs((HashMap<String, Object>) child);
+                res += s[1];
+                size += s[0];
+            } else if (child instanceof Integer) {
+                size += (int) child;
+            }
+        }
+        if(size < 100000){
+            res += size;
+        }
+        return new int[] {size, res};
     }
 
     @Override
     public String part2(List<String> input) {
-        int res = 0;
-        return String.valueOf(res);
+        HashMap<String, Object> root = new HashMap<>();
+        HashMap<String, Object> dirs = root;
+        Stack<HashMap<String, Object>> stack = new Stack<>();
+
+        for (String s : input) {
+            if (s.charAt(0) == '$') {
+                if (s.charAt(2) == 'c') {
+                    String dir = s.substring(5);
+                    if (dir.equals("/")) {
+                        dirs = root;
+                        stack.clear();
+                    } else if (dir.equals("..")) {
+                        dirs = stack.pop();
+                    } else {
+                        if (!dirs.containsKey(dir)) {
+                            dirs.put(dir, new HashMap<String, Object>());
+                        }
+                        stack.push(dirs);
+                        dirs = (HashMap<String, Object>) dirs.get(dir);
+                    }
+                }
+            } else {
+                String[] parts = s.split(" ");
+                String dirOrSize = parts[0];
+                String name = parts[1];
+                if (dirOrSize.equals("dir")) {
+                    if (!dirs.containsKey(name)) {
+                        dirs.put(name, new HashMap<String, Object>());
+                    }
+                } else {
+                    dirs.put(name, Integer.parseInt(dirOrSize));
+                }
+            }
+        }
+        int needToFree = size(root) - 40000000;
+        return String.valueOf(freeSpace(root, needToFree));
     }
+
+    int size(HashMap<String, Object> dir) {
+        int size = 0;
+        for (Object child : dir.values()) {
+            if (child instanceof HashMap) {
+                size += size((HashMap<String, Object>) child);
+            } else if (child instanceof Integer) {
+                size += (int) child;
+            }
+        }
+        return size;
+    }
+
+    int freeSpace(HashMap<String, Object> dir, int space){
+        int res = Integer.MAX_VALUE;
+        if(size(dir) >= space){
+            res = size(dir);
+        }
+        for (Object child : dir.values()){
+            if (child instanceof HashMap) {
+                int resChild = freeSpace((HashMap<String, Object>) child, space);
+                res = Math.min(res, resChild);
+            }
+        }
+        return res;
+    }
+
 }
--- src/AoC/days/day08.java
+++ src/AoC/days/day08.java
@@ -3,24 +3,130 @@ package AoC.days;
 import AoC.day;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Math.max;
 
 public class day08 implements day {
     @Override
     public String part1(List<String> input) {
         int res = 0;
-
+        int[][] forest = new int[input.size()][input.size()];
+        int index_row = 0;
         for(String s: input){
-            String[] sPars = s.trim().split(" ");
+            int index_col = 0;
+            String[] sPars = s.trim().split("");
+            for(String tree: sPars){
+                forest[index_row][index_col] = Integer.parseInt(tree);
+                index_col++;
+            }
+            index_row++;
         }
 
-
+        for (int y = 0; y < input.size(); y++) {
+            for (int x = 0; x < input.size(); x++) {
+                boolean bigger = true;
+                for (int k = y - 1; k >= 0; k--) {
+                    if (forest[k][x] >= forest[y][x]) {
+                        bigger = false;
+                    }
+                }
+                if (bigger) {
+                    res++;
+                    continue;
+                }
+                bigger = true;
+                for (int k = y + 1; k < forest.length; k++) {
+                    if (forest[k][x] >= forest[y][x]) {
+                        bigger = false;
+                    }
+                }
+                if (bigger) {
+                    res++;
+                    continue;
+                }
+                bigger = true;
+                for (int k = x + 1; k < forest[0].length; k++) {
+                    if (forest[y][k] >= forest[y][x]) {
+                        bigger = false;
+                    }
+                }
+                if (bigger) {
+                    res++;
+                    continue;
+                }
+                bigger = true;
+                for (int k = x - 1; k >= 0; k--) {
+                    if (forest[y][k] >= forest[y][x]) {
+                        bigger = false;
+                    }
+                }
+                if (bigger) {
+                    res++;
+                    continue;
+                }
+            }
+        }
         return String.valueOf(res);
     }
 
     @Override
     public String part2(List<String> input) {
         int res = 0;
+        int[][] forest = new int[input.size()][input.size()];
+        int index_row = 0;
+        for(String s: input){
+            int index_col = 0;
+            String[] sPars = s.trim().split("");
+            for(String tree: sPars){
+                forest[index_row][index_col] = Integer.parseInt(tree);
+                index_col++;
+            }
+            index_row++;
+        }
+
+        for (int y = 0; y < input.size(); y++) {
+            for (int x = 0; x < input.size(); x++) {
+                int viewDistance = 1;
+
+                int tempDistance = 0;
+                for (int k = y - 1; k >= 0; k--) {
+                    tempDistance++;
+                    if (forest[k][x] >= forest[y][x]) {
+                        break;
+                    }
+                }
+                viewDistance *= tempDistance;
+                tempDistance = 0;
+                for (int k = y + 1; k < forest.length; k++) {
+                    tempDistance++;
+                    if (forest[k][x] >= forest[y][x]) {
+                        break;
+                    }
+                }
+                viewDistance *= tempDistance;
+                tempDistance = 0;
+                for (int k = x + 1; k < forest[0].length; k++) {
+                    tempDistance++;
+                    if (forest[y][k] >= forest[y][x]) {
+                        break;
+                    }
+                }
+                viewDistance *= tempDistance;
+                tempDistance = 0;
+                for (int k = x - 1; k >= 0; k--) {
+                    tempDistance++;
+                    if (forest[y][k] >= forest[y][x]) {
+                        break;
+                    }
+                }
+                viewDistance *= tempDistance;
+                if (viewDistance > res) {
+                    res = viewDistance;
+                }
+            }
+        }
         return String.valueOf(res);
     }
 }
